caf3c75147f4b52f0786e77738d33b2a9caec63c
Steigen, Savanah <steigen@theinstitutes.org>
2023-01-19 16:07:18 -0500

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { AppBar } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import { InputBase } from "@material-ui/core";
+import { Search } from "@material-ui/icons";
+import { Switch } from "@material-ui/core";
+import { withStyles } from "@material-ui/core";
+
+import styles from "./styles/NavbarStyles";
+
+class Navbar extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span>🇫🇷</span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit">
+              App Title
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <Search />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Navbar);
